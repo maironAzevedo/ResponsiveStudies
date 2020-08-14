62c97fe968da7da52dbe5ed3f1336e30b9ce48ac
maironAzevedo <maironazevedo@id.uff.br>
2020-08-13 23:04:54 -0300

    commit inicial

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,17 @@
-document.querySelector('.menu-controle').onclick = function() {
-	document.documentElement.classList.toggle('menu-ativo');
-};
+const openButton = document.querySelector(".menu-abrir")
+const page = document.querySelector("html")
+const closeButton = document.querySelector(".menu-fechar")
+
+openButton.addEventListener("click", () => {
+    page.classList.add('menu-ativo');
+})
+
+closeButton.addEventListener("click", () => {
+    page.classList.remove('menu-ativo'); 
+})
+
+page.addEventListener("click", () => {
+    if (event.target === page) {
+        page.classList.remove('menu-ativo');
+    }
+})
